a3f11e251d356d08fe579543d8003617ba8ceb81
Mustaeen Siddiqui <mustaeensiddiqui27@gmail.com>
2024-06-05 19:32:47 +0100

    first commit

---
+++ SlantSixStudioBlog/urls.py
@@ -0,0 +1,12 @@
+# blog/urls.py
+
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path("", views.blog_index, name="blog_index"),
+    path("post/<int:pk>/", views.blog_detail, name="blog_detail"),
+    path("category/<category>/", views.blog_category, name="blog_category"),
+]
