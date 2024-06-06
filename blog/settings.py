714192674a5c8d16524276f2117d43a007dec574
Mustaeen Siddiqui <mustaeensiddiqui27@gmail.com>
2024-06-06 13:52:03 +0100

    changed allowed hosts

--- blog/settings.py
+++ blog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-rhw(m%qd7n@au#am^@m*4l59d*78=hacdq%guhv8t*fjjtvp#o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
